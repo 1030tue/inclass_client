5c6072f8c63e619ccb0b85cd4f4046713f0a6d47
Kate Kim <katekim5809@gmail.com>
2019-05-15 12:21:07 -0400

    update

---
+++ src/components/NavBar.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+
+class Nav extends React.Component {
+  render(){
+    return(
+      <div className="topnav">
+        <span>|MENU|</span>
+        <div className="navbar-name">
+            <span> |HOME| </span>
+        </div>
+
+
+      </div>
+    )
+  }
+}
+
+export default Nav
---
+++ src/containers/ClassPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+
+class ClassPage extends React.Component {
+  render(){
+    return(
+    <div>ClassPage in Containers folder</div>
+    )
+  }
+}
+
+export default ClassPage
